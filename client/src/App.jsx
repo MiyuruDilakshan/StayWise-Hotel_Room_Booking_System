8be66e8f86f6529a52103c91a31720d94f68aa19
Harin-Gayantha <haringayantha@gmail.com>
2025-12-10 22:20:43 +0530

    Add Rooms page component and update routing in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,12 @@ import AdminLayout from './layouts/AdminLayout'
 
 // Import pages
 import Home from './pages/Home'
+import Rooms from './pages/Rooms'
 import AdminDashboard from './pages/admin/AdminDashboard'
 import AdminLogin from './pages/admin/AdminLogin'
 import AdminForgotPassword from './pages/admin/AdminForgotPassword'
-import EditRoom from "./pages/admin/EditRoom"; 
-import Bookings from "./pages/admin/Bookings"; 
+import EditRoom from "./pages/admin/EditRoom";
+import Bookings from "./pages/admin/Bookings";
 import Users from "./pages/admin/Users";
 import Settings from "./pages/admin/Settings";
 
@@ -19,8 +20,8 @@ function Placeholder({ name }) {
   return (
     <div style={{ padding: 40, textAlign: 'center' }}>
       <h2>{name}</h2>
-      <p>This page is ready to be implemented by the team.<br/>
-        when you make pages in client/src/pages make sure to import pages in app.jsx(client\src\App.jsx) <br/>
+      <p>This page is ready to be implemented by the team.<br />
+        when you make pages in client/src/pages make sure to import pages in app.jsx(client\src\App.jsx) <br />
         example - add " import Login from './pages/Login' " in under "// Import pages"
       </p>
     </div>
@@ -33,7 +34,7 @@ export default function App() {
       {/* Customer Routes WITH navbar & footer */}
       <Route element={<MainLayout />}>
         <Route path="/" element={<Home />} />
-        <Route path="/rooms" element={<Placeholder name="Browse Rooms" />} />
+        <Route path="/rooms" element={<Rooms />} />
         <Route path="/about" element={<Placeholder name="About Us" />} />
         <Route path="/contact" element={<Placeholder name="Contact Us" />} />
         <Route path="/login" element={<Placeholder name="Login" />} />
@@ -49,10 +50,10 @@ export default function App() {
         <Route path="/admin/dashboard" element={<AdminDashboard />} />
         <Route path="/admin/rooms" element={<Placeholder name="Manage Rooms" />} />
         <Route path="/admin/bookings" element={<Bookings />} />
-        <Route path="/admin/users" element={<Users/>} />
+        <Route path="/admin/users" element={<Users />} />
         <Route path="/admin/settings" element={<Settings />} />
         <Route path="/admin/rooms/edit/:id" element={<EditRoom />} />
-        
+
 
       </Route>
     </Routes>
